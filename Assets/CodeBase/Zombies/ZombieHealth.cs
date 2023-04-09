166b432f1a73d5380e160937ac0bf33d055cde32
jumanjiggs <88338482+jumanjiggs@users.noreply.github.com>
2023-04-09 14:26:52 +0300

    fixes

--- Assets/CodeBase/Zombies/ZombieHealth.cs
+++ Assets/CodeBase/Zombies/ZombieHealth.cs
@@ -1,5 +1,6 @@
 using CodeBase.Services;
 using Opsive.UltimateCharacterController.Traits;
+using Opsive.UltimateCharacterController.Traits.Damage;
 using UnityEngine;
 using Zenject;
 
@@ -11,21 +12,36 @@ namespace CodeBase.Zombies
         public Animator zombieAnimator;
         public Collider zombieCollider;
         [Inject] private KillsCounter m_KillsCounter;
-        
+        [SerializeField] private ParticleSystem fxHit;
+
+
         public override void Die(Vector3 position, Vector3 force, GameObject attacker)
         {
             base.Die(position, force, attacker);
             SwitchRagdollState(false);
             m_KillsCounter.ZombieDie();
         }
-        
+
+
+        public override void OnDamage(DamageData damageData)
+        {
+            base.OnDamage(damageData);
+            PlayFxOnHit();
+        }
+
+        private void PlayFxOnHit()
+        {
+            Instantiate(fxHit.gameObject,
+                new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z),
+                Quaternion.identity, transform);
+        }
+
         public void SwitchRagdollState(bool activate)
         {
             agent.ResetAttack();
             zombieAnimator.enabled = activate;
             zombieCollider.enabled = activate;
             agent.enabled = activate;
-            
         }
     }
 }
